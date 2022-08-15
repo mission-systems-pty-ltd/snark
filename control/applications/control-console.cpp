3c462d2bfcaa329fc3a3f0d35bebd7b3fd71b612
vsevolod vlaskine <vsevolod.vlaskine@gmail.com>
2022-08-15 12:45:13 +1000

    control-console: default output made ascii

--- control/applications/control-console.cpp
+++ control/applications/control-console.cpp
@@ -23,11 +23,13 @@ static void _layout( std::ostream& os = std::cerr )
 static void usage( bool verbose )
 {
     std::cerr << std::endl;
-    std::cerr << "usage: io-console | control-console | my-app-using-poses-on-stdin --binary=6d" << std::endl;
+    std::cerr << "usage" << std::endl;
+    std::cerr << "    io-console | control-console | my-app-using-poses-on-stdin" << std::endl;
+    std::cerr << "    io-console | control-console --binary 6f | my-app-using-poses-on-stdin --binary=6f" << std::endl;
     std::cerr << std::endl;
     std::cerr << "input: key codes as binary bytes; same as io-console output" << std::endl;
     std::cerr << std::endl;
-    std::cerr << "output: x,y,z,roll,pitch,yaw binary records; format: 6d" << std::endl;
+    std::cerr << "output: x,y,z,roll,pitch,yaw" << std::endl;
     std::cerr << std::endl;
     std::cerr << "keys" << std::endl;
     _layout();
@@ -39,6 +41,9 @@ static void usage( bool verbose )
     std::cerr << "    --step,-s=<meters>; default=1; linear increment step" << std::endl;
     std::cerr << "    --step-angle,-a=<radians>; default=0.0873; angle increment step, default: 5 degrees" << std::endl;
     std::cerr << "    --verbose,-v: print hot keys and pose on stderr" << std::endl;
+    std::cerr << std::endl;
+    std::cerr << "csv-options" << std::endl;
+    std::cerr << "    --binary=[<format>]; output pose as binary" << std::endl;
     std::cerr << std::endl;
     exit( 0 );
 }
@@ -69,14 +74,13 @@ int main( int ac, char** av )
     try
     {
         comma::command_line_options options( ac, av, usage );
-        comma::csv::options csv;
+        comma::csv::options csv( options );
+        csv.flush = true;
         double step = options.value( "--step,-s", 1. );
         double angle = options.value( "--step-angle,-a", 5 * M_PI / 180 );
         bool lookaround = options.exists( "--lookaround,-l" );
         bool verbose = options.exists( "--verbose,-v" );
         if( verbose ) { std::cerr << std::endl; _layout(); std::cerr << std::endl; }
-        csv.format( "6d" );
-        csv.flush = true;
         comma::csv::output_stream< snark::position > ostream( std::cout, csv );
         snark::position pose = comma::csv::ascii< snark::position >().get( options.value< std::string >( "--pose,-p", "0,0,0,0,0,0" ) );
         _output( ostream, pose, lookaround, verbose );
