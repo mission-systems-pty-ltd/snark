2b7c2deace35b8eb6f83084e243c15e27a4a0430
vsevolod vlaskine <vsevolod.vlaskine@gmail.com>
2022-08-16 13:52:47 +1000

    math-fft: --linear: made default behavior; --logarithmic added (used to be default behavior)

--- math/applications/math-fft.cpp
+++ math/applications/math-fft.cpp
@@ -1,36 +1,9 @@
-// This file is part of snark, a generic and flexible library for robotics research
 // Copyright (c) 2011 The University of Sydney
-// All rights reserved.
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are met:
-// 1. Redistributions of source code must retain the above copyright
-//    notice, this list of conditions and the following disclaimer.
-// 2. Redistributions in binary form must reproduce the above copyright
-//    notice, this list of conditions and the following disclaimer in the
-//    documentation and/or other materials provided with the distribution.
-// 3. Neither the name of the University of Sydney nor the
-//    names of its contributors may be used to endorse or promote products
-//    derived from this software without specific prior written permission.
-//
-// NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
-// GRANTED BY THIS LICENSE.  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
-// HOLDERS AND CONTRIBUTORS \"AS IS\" AND ANY EXPRESS OR IMPLIED
-// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
-// BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-// OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
-// IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#include <vector>
 #include <iostream>
 #include <iterator>
 #include <math.h>
+#include <vector>
 #include <boost/optional.hpp>
 #include <fftw3.h>
 #include <comma/visiting/traits.h>
@@ -38,19 +11,20 @@
 #include <comma/application/verbose.h>
 #include <comma/csv/options.h>
 #include <comma/csv/stream.h>
+#include <comma/string/string.h>
 #include "detail/shuffle-tied.h"
 
-std::size_t input_size=0;
-bool filter_input=true;
-bool logarithmic_output=true;
-bool magnitude=false;
-bool real=false;
-bool split=false;
-std::size_t bin_size=0;
-boost::optional<double> bin_overlap;
-bool tied=true;
+static std::size_t input_size=0;
+static bool filter_input=true;
+static bool logarithmic_output=true;
+static bool magnitude=false;
+static bool real=false;
+static bool split=false;
+static std::size_t bin_size=0;
+static boost::optional<double> bin_overlap;
+static bool tied=true;
 
-void usage(bool detail)
+static void usage( bool detail )
 {
     std::cerr<<"    perform fft on input data" << std::endl;
     std::cerr << std::endl;
@@ -65,7 +39,7 @@ void usage(bool detail)
     std::cerr << std::endl;
     std::cerr << "    --bin-size=[<size>]: cut data into several bins of this size and perform fft on each bin, when not speificed calculates fft on the whole data" << std::endl;
     std::cerr << "    --bin-overlap=[<overlap>]: if specified, each bin will contain this portion of the last bin's data, range: 0 (no overlap) to 1"<<std::endl;
-    std::cerr << "    --linear: output as linear; when not specified, output will be scaled to lograithm of 10 for magnitude or real part (phase is not affected)" << std::endl;
+    std::cerr << "    --logarithmic,--log: scale output to logarithm of 10 for magnitude or real part (phase is not affected)" << std::endl;
     std::cerr << "    --no-filter: when not specified, filters input using a cut window to get limited output" << std::endl;
     std::cerr << "    --size=<size>: size of input vector" << std::endl;
     std::cerr << std::endl;
@@ -77,26 +51,18 @@ void usage(bool detail)
     std::cerr << "    --split: output array of real followed by array of complex part; when not specified real and complex parts are interleaved" << std::endl;
     std::cerr << std::endl;
     std::cerr << "    --shuffle,--shuffle-fields,--shuffled-fields=[<csv_fields>]: comma separated list of input fields to be written to stdout; if not specified prepend all input fields to the output" << std::endl;
-    std::cerr << "    --untied: only write output (doesn't write input to stdout)" << std::endl;
+    std::cerr << "    --untied,--discard-input: only write output (doesn't write input to stdout)" << std::endl;
     std::cerr << std::endl;
     std::cerr << "  utility options:" << std::endl;
     std::cerr << "    --output-fields: print output fields and exit; depends on input fields and size" << std::endl;
     std::cerr << "    --output-format: print binary format of output and exit; depends on input fields and size" << std::endl;
     std::cerr << "    --output-size: print size of output data array and exit; e.g. if output format is t,ui,256d, then output size is 256" << std::endl;
     std::cerr << std::endl;
-    if(detail)
-    {
-        std::cerr << "csv options:" << std::endl;
-        std::cerr<< comma::csv::options::usage() << std::endl;
-        std::cerr << std::endl;
-    }
-    else
-    {
-        std::cerr << "use -v or --verbose to see more detail" << std::endl;
-        std::cerr << std::endl;
-    }
+    std::cerr << "csv options:" << std::endl;
+    std::cerr << comma::csv::options::usage( detail ) << std::endl;
+    std::cerr << std::endl;
     std::cerr << "example" << std::endl;
-    std::cerr << "      " << comma::verbose.app_name() << " --binary=\"t,16000f\" --fields=t,data --size=16000" << std::endl;
+    std::cerr << "    math-fft --binary=\"t,16000f\" --fields=t,data --size=16000" << std::endl;
     std::cerr << std::endl;
     exit(0);
 }
@@ -107,44 +73,35 @@ void usage(bool detail)
 struct input_t
 {
     std::vector<double> data;
+    
     input_t() : data(input_size) {}
 };
 
 struct output_t
 {
     std::vector<double> data;
+    
     output_t()
     {
         std::size_t len=bin_size;
         if(magnitude || real) { len/=2; }
         data.resize(len);
     }
-    void reset()
-    {
-        memset(&data[0],0,data.size()*sizeof(data[0]));
-    }
+    
+    void reset() { std::memset(&data[0],0,data.size()*sizeof(data[0])); }
 };
 
 namespace comma { namespace visiting {
 
 template <> struct traits< input_t >
 {
-    template< typename K, typename V > static void visit( const K& k, input_t& p, V& v )
-    {
-        v.apply( "data", p.data );
-    }
-    template< typename K, typename V > static void visit( const K& k, const input_t& p, V& v )
-    {
-        v.apply( "data", p.data );
-    }
+    template< typename K, typename V > static void visit( const K& k, input_t& p, V& v ) { v.apply( "data", p.data ); }
+    template< typename K, typename V > static void visit( const K& k, const input_t& p, V& v ) { v.apply( "data", p.data ); }
 };
 
 template <> struct traits< output_t >
 {
-    template< typename K, typename V > static void visit( const K& k, const output_t& p, V& v )
-    {
-        v.apply( "output", p.data );
-    }
+    template< typename K, typename V > static void visit( const K& k, const output_t& p, V& v ) { v.apply( "output", p.data ); }
 };
 
 } } // namespace comma { namespace visiting {
@@ -169,13 +126,16 @@ struct fft
     {
         for( std::size_t i = 0; i < size; ++i ) { h[i] = 0.54 - 0.46 * std::cos( M_PI * 2 * i / size ); }
     }
+    
     ~fft()
     {
         fftw_destroy_plan(plan);
         fftw_free( input ); // seems that fftw_destroy_plan() releases it
         fftw_free( output ); // seems that fftw_destroy_plan() releases it
     }
+    
     void calculate() { fftw_execute( plan); }
+    
     std::size_t output_size() const { return h.size() / 2; }
 };
 
@@ -233,9 +193,6 @@ void calculate(const double* data, std::size_t size, std::vector<double>& output
 
 struct app
 {
-    app()
-    {
-    }
     void process(const comma::csv::options& csv, const boost::optional<std::string>& shuffle_fields)
     {
         if(bin_overlap && int(bin_size*(1-*bin_overlap)) <= 0) { COMMA_THROW( comma::exception, "bin size and overlap don't work" ); }
@@ -248,47 +205,27 @@ struct app
         output_t output;
         while(std::cin.good())
         {
-            //read a record
             const input_t* input=is.read();
             if(!input) { break; }
-            //calculate
             for(std::size_t bin_offset=0;bin_offset<input_size;bin_offset+=(bin_overlap ? bin_size*(1-*bin_overlap) : bin_size))
             {
                 output.reset();
                 calculate(&input->data[bin_offset], std::min(bin_size,input_size-bin_offset), output.data);
-                //write output
-                if(tied)
-                    shuffle_tied.append(output);
-                else
-                    os.write(output);
+                if( tied ) { shuffle_tied.append( output ); } else { os.write(output); }
             }
         }
     }
-    std::size_t get_output_size()
-    {
-        return output_t().data.size();
-    }
-    void output_format()
-    {
-        std::cout<<output_t().data.size()<<"d"<<std::endl;
-    }
-    void output_fields()
-    {
-        std::cout<<comma::join(comma::csv::names< output_t >(true),',')<<std::endl;
-    }
+    
+    std::size_t get_output_size() const { return output_t().data.size(); }
+    
+    void output_format() const { std::cout<<output_t().data.size()<<"d"<<std::endl; }
+    
+    void output_fields() { std::cout<<comma::join(comma::csv::names< output_t >(true),',')<<std::endl; }
 };
 
-template<typename T>
-std::ostream& operator<< (std::ostream& o, const std::vector<T>& v)
-{
-    std::copy(v.begin(), v.end(), std::ostream_iterator<T>(o, " "));
-    return o;
-}
-template<typename T>
-void range_check(T value, T min, T max, const char* label)
-{
-    if(value<min || value>max) { COMMA_THROW(comma::exception, label<<" out of range "<<min<<" to "<<max ); }
-}
+template< typename T > static std::ostream& operator<< (std::ostream& os, const std::vector<T>& v) { std::copy(v.begin(), v.end(), std::ostream_iterator<T>(os, " ")); return os; }
+
+template< typename T > static void range_check(T value, T min, T max, const char* label) { if(value<min || value>max) { COMMA_THROW( comma::exception, label << " out of range " << min << " to " << max ); } }
 
 int main( int argc, char** argv )
 {
@@ -297,36 +234,30 @@ int main( int argc, char** argv )
     {
         comma::csv::options csv(options,"data");
         csv.full_xpath = false;
-        filter_input= ! options.exists("--no-filter");
-        logarithmic_output= ! options.exists("--linear");
-        input_size=options.value<std::size_t>("--size");
-        magnitude=options.exists("--magnitude");
-        real=options.exists("--real");
-        split=options.exists("--split");
-        bin_size=options.value<std::size_t>("--bin-size", input_size);
-        range_check<std::size_t>(bin_size,0,input_size,"bin_size");
+        filter_input = !options.exists( "--no-filter" );
+        logarithmic_output = options.exists( "--logarithmic,--log" );
+        input_size = options.value< std::size_t >( "--size" );
+        magnitude = options.exists( "--magnitude" );
+        real = options.exists( "--real" );
+        split = options.exists( "--split" );
+        bin_size = options.value<std::size_t>( "--bin-size", input_size );
+        range_check< std::size_t >( bin_size, 0, input_size, "bin_size" );
         bin_overlap=options.optional<double>("--bin-overlap");
         if(bin_overlap) { range_check<double>(*bin_overlap,0,1,"bin_overlap"); }
-        boost::optional<std::string> shuffle_fields=options.optional<std::string>("--shuffle,--shuffle-fields,--shuffled-fields");
-        tied=!options.exists("--untied");
-        if(shuffle_fields&&!tied) { COMMA_THROW(comma::exception,"--shuffle only works with tied stream (can't specify both --untied and --shuffle"); }
-        std::vector<std::string> unnamed=options.unnamed("--verbose,-v,--output-size,--output-format,--output-fields,--no-filter,--linear,--magnitude,--real,--split,--untied", 
-                                                         "--binary,-b,--fields,-f,--delimiter,-d,--size,--bin-size,--bin-overlap,--shuffle,--shuffle-fields,--shuffled-fields");
-        if(unnamed.size() != 0) { COMMA_THROW(comma::exception, "invalid option(s): " << unnamed ); }
+        boost::optional< std::string > shuffle_fields = options.optional< std::string >( "--shuffle,--shuffle-fields,--shuffled-fields" );
+        tied = !options.exists( "--untied,--discard-input" );
+        options.assert_mutually_exclusive( "--shuffle,--shuffle-fields,--shuffled-fields", "--untied,--discard-input" );
+        std::vector< std::string > unnamed = options.unnamed( "--verbose,-v,--output-size,--output-format,--output-fields,--no-filter,--logarithmic,--log,--magnitude,--real,--split,--untied,--discard-input"
+                                                            , "--binary,-b,--fields,-f,--delimiter,-d,--size,--bin-size,--bin-overlap,--shuffle,--shuffle-fields,--shuffled-fields" );
+        if( unnamed.size() > 0 ) { comma::say() << "invalid option(s): " << comma::join( unnamed, ' ' ) << std::endl; return 1; }
         app app;
         if(options.exists("--output-size")) { std::cout<< app.get_output_size() << std::endl; return 0; }
         if(options.exists("--output-format")) { app.output_format(); return 0; }
         if(options.exists("--output-fields")) { app.output_fields(); return 0; }
-        app.process(csv,shuffle_fields);
+        app.process( csv, shuffle_fields );
         return 0;
     }
-    catch( std::exception& ex )
-    {
-        std::cerr << comma::verbose.app_name() << ": " << ex.what() << std::endl;
-    }
-    catch( ... )
-    {
-        std::cerr << comma::verbose.app_name() << ": " << "unknown exception" << std::endl;
-    }
+    catch( std::exception& ex ) { comma::say() << ": " << ex.what() << std::endl; }
+    catch( ... ) { comma::say() << ": " << "unknown exception" << std::endl; }
     return 1;
 }
