70bbc6a9e60f02769de7658446be9d37dde60084
vsevolod vlaskine <vsevolod.vlaskine@gmail.com>
2022-08-22 18:16:50 +1000

    points-mesh: minor brush-up

--- point_cloud/applications/points-mesh.cpp
+++ point_cloud/applications/points-mesh.cpp
@@ -1,31 +1,4 @@
-// This file is part of snark, a generic and flexible library for robotics research
 // Copyright (c) 2011 The University of Sydney
-// All rights reserved.
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are met:
-// 1. Redistributions of source code must retain the above copyright
-//    notice, this list of conditions and the following disclaimer.
-// 2. Redistributions in binary form must reproduce the above copyright
-//    notice, this list of conditions and the following disclaimer in the
-//    documentation and/or other materials provided with the distribution.
-// 3. Neither the name of the University of Sydney nor the
-//    names of its contributors may be used to endorse or promote products
-//    derived from this software without specific prior written permission.
-//
-// NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
-// GRANTED BY THIS LICENSE.  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
-// HOLDERS AND CONTRIBUTORS \"AS IS\" AND ANY EXPRESS OR IMPLIED
-// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
-// BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-// OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
-// IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 /// @author vsevolod vlaskine
 
@@ -63,12 +36,10 @@ static void usage( bool verbose = false )
     std::cerr << "options" << std::endl;
     std::cerr << "    --help,-h:       show this help; --help --verbose for more help" << std::endl;
     std::cerr << "    --verbose,-v:    more output" << std::endl;
-    if( verbose )
-    {
-        std::cerr << std::endl;
-        std::cerr << "csv options" << std::endl;
-        std::cerr << comma::csv::options::usage() << std::endl;
-    }
+    std::cerr << std::endl;
+    std::cerr << "csv options" << std::endl;
+    std::cerr << comma::csv::options::usage( "x,y", verbose ) << std::endl;
+    std::cerr << std::endl;
     exit( 0 );
 }
 
@@ -116,7 +87,7 @@ class grid
             , istream_( std::cin, csv_ )
             , permissive_( options.exists( "--permissive" ) )
             , reverse_( options.exists( "--reverse" ) )
-            , voxel_map_( resolution_( options.value< double >( "--resolution", 1 ) ) ) // quick and dirty: does not matter for now, but may be used in future
+            , voxel_map_( resolution_( options.value( "--resolution", 1. ) ) ) // quick and dirty: does not matter for now, but may be used in future
         {
         }
         
@@ -192,13 +163,11 @@ class grid
                     i[0] = it->first[0];
                     i[1] = it->first[1] - 1;
                     iterator_ down = voxel_map_.find( i );
-                    //if there is no down
-                    if(down==voxel_map_.end()) 
+                    if( down == voxel_map_.end() ) // if there is no down
                     {
                         i[0] = it->first[0] + 1;
                         i[1] = it->first[1] - 1;
-                        iterator_ right_down = voxel_map_.find( i );
-                        //but there is right_down
+                        iterator_ right_down = voxel_map_.find( i ); // but there is right_down
                         if( right_down != voxel_map_.end() ) { output_( it, right, right_down ); }
                     }
                 }
