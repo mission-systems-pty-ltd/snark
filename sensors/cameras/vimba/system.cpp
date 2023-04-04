a533824dceedfaef03e716a886f23c69c6cec8fc
Dave Jennings <dave.jennings@missionsystems.com.au>
2023-04-04 12:27:08 +1000

    vimba/system.cpp: disable output to stdout when calling SDK

--- sensors/cameras/vimba/system.cpp
+++ sensors/cameras/vimba/system.cpp
@@ -1,20 +1,49 @@
 // This file is part of snark, a generic and flexible library for robotics research
 // Copyright (c) 2016 The University of Sydney
+// Copyright (c) 2023 Mission Systems Pty Ltd
 
-#include <comma/base/exception.h>
 #include "error.h"
 #include "system.h"
+#include <comma/base/exception.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
 
 namespace snark { namespace vimba {
 
 AVT::VmbAPI::VimbaSystem& system::instance = AVT::VmbAPI::VimbaSystem::GetInstance();
 
+// In Vimba 6.1 (and probably other versions ) AVT::VmbAPI::VimbaSystem::Startup()
+// outputs a '\n' byte to stdout. This breaks any sort of image processing pipeline
+// (since output_frame also outputs to stdout).
+//
+// We fix this by redirecting stdout to /dev/null whilst calling the SDK (just here).
+//
+// You will know if the SDK exhibits this behaviour if (without the below fix)
+// there's an extra line with --list-cameras. e.g.
+// $ vimba-cat --list-cameras
+//
+// id="<dev-id>",name="Allied Vision 1800 C-507c",serial="<serial-num>",interface_type="CSI2",interface_id="VimbaCSIInterface_0x0"
+// $
 system::system()
 {
+    // see https://stackoverflow.com/questions/4832603/how-could-i-temporary-redirect-stdout-to-a-file-in-a-c-program
+    // temporarily redirect stdout to /dev/null
+    fflush( stdout );
+    int orig = dup( 1 );                // save current stdout fd
+    int devnull = open( "/dev/null", O_WRONLY ); // create fd for /dev/null
+    dup2( devnull, 1 );                 // copy devnull fd to stdout fd (effectively assigning stdout to /dev/null)
+    close( devnull );                   // we don't need this any more now that it's copied
+
+    // calls AVT::VmbAPI::VimbaSystem::Startup() which outputs byte to stdout
     VmbErrorType status = instance.Startup();
-    if( status != VmbErrorSuccess ) {
-        COMMA_THROW( comma::exception, error_msg( "Failed to start API", status ));
-    }
+
+    // restore stdout
+    fflush( stdout );
+    dup2( orig, 1 );                    // copy our saved fd to stdout fd (effectively restoring stdout to original setting)
+    close( orig );                      // we don't need this any more
+
+    if( status != VmbErrorSuccess ) { COMMA_THROW( comma::exception, error_msg( "Failed to start API", status )); }
 }
 
 VmbVersionInfo_t system::vmb_version()
