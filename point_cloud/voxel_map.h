ff573cd4e4d393e1376e8727cf7309e22c5a721a
vsevolod vlaskine <vsevolod.vlaskine@gmail.com>
2023-06-06 16:04:53 +1000

    point_cloud/voxel_map: refactored and simplified

--- point_cloud/voxel_map.h
+++ point_cloud/voxel_map.h
@@ -11,90 +11,14 @@
 #include <comma/base/types.h>
 #include <comma/containers/multidimensional/map.h>
 
-namespace comma { namespace impl {
-
-template < typename T, std::size_t Size > struct array_traits< Eigen::Matrix< T, Size, 1 > >
-{
-    enum { size = Size };
-
-    typedef Eigen::Matrix< T, Size, 1 > vector_t;
-
-    static vector_t subtract( const vector_t& lhs, const vector_t& rhs ) { return lhs - rhs; }
-
-    static vector_t divide( const vector_t& lhs, const vector_t& rhs ) { return vector_t( lhs.array() / rhs.array() ); }
-
-    static vector_t zero() { return vector_t::Zero(); }
-};
-
-} } // namespace comma { namespace impl {
-
 namespace snark {
 
-// /// quick and dirty hash for array-like containers (its support is awkward in boost)
-// /// @todo if we have a second use case, move to ark::containers... i guess...
-// template < typename Array, std::size_t Size >
-// struct array_hash : public std::unary_function< Array, std::size_t >
-// {
-//     std::size_t operator()( Array const& array ) const
-//     {
-//         std::size_t seed = 0;
-//         for( std::size_t i = 0; i < Size; ++i ) { boost::hash_combine( seed, array[i] ); }
-//         return seed;
-//         // return boost::hash_range( &array[0], &array[Size] ); // not so easy...
-//     }
-// };
-
-namespace impl {
-
-template < typename T > struct vector_traits;
-
-// template < typename T, std::size_t Size > struct vector_traits< Eigen::Matrix< T, Size, 1 > >
-// {
-//     enum { size = Size };
-
-//     typedef Eigen::Matrix< T, Size, 1 > vector_t;
-
-//     static vector_t subtract( const vector_t& lhs, const vector_t& rhs ) { return lhs - rhs; }
-
-//     static vector_t divide( const vector_t& lhs, const vector_t& rhs ) { return vector_t( lhs.array() / rhs.array() ); }
-
-//     static vector_t zero() { return vector_t::Zero(); }
-// };
-
-template <> struct vector_traits< Eigen::Vector2d > // todo: trivially generalize to Eigen::Matrix< T, Size, 1 >
-{
-    enum { size = 2 };
-
-    typedef Eigen::Vector2d vector_t;
-
-    static vector_t subtract( const vector_t& lhs, const vector_t& rhs ) { return lhs - rhs; }
-
-    static vector_t divide( const vector_t& lhs, const vector_t& rhs ) { return vector_t( lhs.array() / rhs.array() ); }
-
-    static vector_t zero() { return vector_t::Zero(); }
-};
-
-template <> struct vector_traits< Eigen::Vector3d >
-{
-    enum { size = 3 };
-
-    typedef Eigen::Vector3d vector_t;
-
-    static vector_t subtract( const vector_t& lhs, const vector_t& rhs ) { return lhs - rhs; }
-
-    static vector_t divide( const vector_t& lhs, const vector_t& rhs ) { return vector_t( lhs.array() / rhs.array() ); }
-
-    static vector_t zero() { return vector_t::Zero(); }
-};
-
-} // namespace impl {
-
-template < typename V, unsigned int D, typename F = double, typename P = Eigen::Matrix< F, D, 1 > >
-class voxel_map : public comma::containers::multidimensional::map< F, V, D, P, impl::vector_traits< P > >
+template < typename V, unsigned int D, typename F = double, typename P = Eigen::Matrix< F, D, 1 > >//class voxel_map : public comma::containers::multidimensional::map< F, V, D, P, impl::vector_traits< P > >
+class voxel_map : public comma::containers::multidimensional::map< F, V, D, P >
 {
     public:
         /// base class type
-        typedef comma::containers::multidimensional::map< F, V, D, P, impl::vector_traits< P > > base_type;
+        typedef comma::containers::multidimensional::map< F, V, D, P > base_type;
 
         /// number of dimensions
         enum { dimensions = D };
