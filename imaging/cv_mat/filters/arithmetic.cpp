eb82ffbc0cc9c0837f43403b87057fdda39a3cdf
vsevolod vlaskine <vsevolod.vlaskine@gmail.com>
2023-09-17 12:50:48 +1000

    imaging/cv_mat/filters/arithmetic: overlay, underlay: todo

--- imaging/cv_mat/filters/arithmetic.cpp
+++ imaging/cv_mat/filters/arithmetic.cpp
@@ -1,31 +1,5 @@
-// This file is part of snark, a generic and flexible library for robotics research
 // Copyright (c) 2011 The University of Sydney
 // All rights reserved.
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are met:
-// 1. Redistributions of source code must retain the above copyright
-//    notice, this list of conditions and the following disclaimer.
-// 2. Redistributions in binary form must reproduce the above copyright
-//    notice, this list of conditions and the following disclaimer in the
-//    documentation and/or other materials provided with the distribution.
-// 3. Neither the name of the University of Sydney nor the
-//    names of its contributors may be used to endorse or promote products
-//    derived from this software without specific prior written permission.
-//
-// NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
-// GRANTED BY THIS LICENSE.  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
-// HOLDERS AND CONTRIBUTORS \"AS IS\" AND ANY EXPRESS OR IMPLIED
-// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
-// BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-// OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
-// IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include <iostream>
 #include <string>
@@ -44,7 +18,9 @@ typename arithmetic< H >::operation arithmetic< H >::str_to_operation(const std:
     else if( s == "subtract" ) { return operation::subtract; }
     else if( s == "add" ) { return operation::add; }
     else if( s == "absdiff" ) { return operation::absdiff; }
-    else { COMMA_THROW(comma::exception, "unknown arithmetic operation: \"" << s << "\", expected: multiply, add, subtract or divide" ); }
+    else if( s == "overlay" ) { return operation::overlay; }
+    else if( s == "underlay" ) { return operation::underlay; }
+    else { COMMA_THROW(comma::exception, "expected arithmetic operation; got: \"" << s << "\"" ); }
 }
 
 template < typename H >
@@ -57,9 +33,10 @@ typename std::string arithmetic< H >::operation_to_str(arithmetic< H >::operatio
         case operation::subtract: return "subtract";
         case operation::add:      return "add";
         case operation::absdiff:  return "absdiff";
+        case operation::overlay:  return "overlay";
+        case operation::underlay:  return "underlay";
     }
-    
-    COMMA_THROW(comma::exception, "arithmetic: unknown operation: " << int(op));
+    COMMA_THROW( comma::exception, "arithmetic: expected operation code; got: '" << int( op ) << "'" );
 }
 
 template < typename H > arithmetic< H >::arithmetic( operation op ) : operation_( op ) {}
@@ -77,13 +54,15 @@ template < typename H >
 typename arithmetic< H >::value_type arithmetic< H >::apply_(const value_type& m, const cv::Mat& operand )
 {
     value_type n( m.first, cv::Mat(m.second.rows, m.second.cols, m.second.type()) );
-    switch(operation_)
+    switch( operation_ )
     {
         case operation::multiply: cv::multiply(m.second, operand, n.second, 1.0, m.second.type() ); break;
         case operation::divide:   cv::divide(m.second, operand, n.second, 1.0, m.second.type() ); break;
         case operation::subtract: cv::subtract(m.second, operand, n.second, cv::noArray(), m.second.type() ); break;
         case operation::add:      cv::add(m.second, operand, n.second, cv::noArray(), m.second.type() );    break;
         case operation::absdiff:  cv::absdiff(m.second, operand, n.second); break;
+        case operation::overlay:  COMMA_THROW( comma::exception, "overlay: todo" ); break;
+        case operation::underlay: COMMA_THROW( comma::exception, "underlay: todo" ); ; break;
     }
     return n;
 }
