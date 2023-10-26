ec78ffa1fc2ae24bec012ed363a41bab6eeff741
Aspen Eyers <aspen.eyers@missionsystems.com.au>
2023-10-26 18:50:49 +1100

    ros/applications/ros2-points: Adding ros2 for pointclouds - Trying to keep compatiable between ros1 and ros2 where possible. This means there are ROS1-2 macros and requirements on checking version all around the place... Also, the ros2 build appears to be SUPER slow, so iv'e had to filter the ros2 link libraries which takes the number of libraries (including duplicates) down from about 760 to 30 odd. This seems fragile, especially if lib1 depends on lib2 which bepends on lib1 etc...

---
+++ ros/version.h
@@ -0,0 +1,20 @@
+#pragma once
+
+// If these are not defined, ROS_VERSION_MAJOR, ROS_VERSION_MINOR, ROS_VERSION_PATCH
+// you must define them in your CMakeLists.txt
+// ROS2 does not define the same macros as ros1 so we need to define them ourselves.
+#ifndef ROS_VERSION_MAJOR
+#error ROS_VERSION_MAJOR not defined. If ROS1, source setup.bash, if ros2, then add add_compile_definitions( ROS_VERSION_MAJOR=XX ),add_compile_definitions( ROS_VERSION_MINOR=XX ),add_compile_definitions( ROS_VERSION_PATCH=XX ) to your CMakeLists.txt. Where; ardent={2,1,0}, bouncy={2,2,0}, crystal={2,3,0} etc. 
+#endif
+#ifndef ROS_VERSION_MINOR
+#error ROS_VERSION_MINOR not defined. If ROS1, source setup.bash, if ros2, then add add_compile_definitions( ROS_VERSION_MAJOR=XX ),add_compile_definitions( ROS_VERSION_MINOR=XX ),add_compile_definitions( ROS_VERSION_PATCH=XX ) to your CMakeLists.txt. Where; ardent={2,1,0}, bouncy={2,2,0}, crystal={2,3,0} etc. 
+#endif
+#ifndef ROS_VERSION_PATCH
+#error ROS_VERSION_PATCH not defined. If ROS1, source setup.bash, if ros2, then add add_compile_definitions( ROS_VERSION_MAJOR=XX ),add_compile_definitions( ROS_VERSION_MINOR=XX ),add_compile_definitions( ROS_VERSION_PATCH=XX ) to your CMakeLists.txt. Where; ardent={2,1,0}, bouncy={2,2,0}, crystal={2,3,0} etc. 
+#endif
+
+#define ROS_VERSION_COMBINED(major, minor, patch) (((major) << 20) | ((minor) << 10) | (patch))
+#define ROS_VERSION ROS_VERSION_COMBINED(ROS_VERSION_MAJOR, ROS_VERSION_MINOR, ROS_VERSION_PATCH)
+
+#define ROS_VERSION_GE(major1, minor1, patch1, major2, minor2, patch2) (ROS_VERSION_COMBINED(major1, minor1, patch1) >= ROS_VERSION_COMBINED(major2, minor2, patch2))
+#define ROS_VERSION_MINIMUM(major, minor, patch) ROS_VERSION_GE(ROS_VERSION_MAJOR, ROS_VERSION_MINOR, ROS_VERSION_PATCH, major, minor, patch)
