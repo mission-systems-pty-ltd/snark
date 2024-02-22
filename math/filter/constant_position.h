6bca13c936b46b10255868e63beda1831a2d2161
seva <seva@tinbox>
2024-02-22 14:23:51 +1100

    math/filter: constant_speed and constant_position models: brushed up

--- math/filter/constant_position.h
+++ math/filter/constant_position.h
@@ -27,57 +27,46 @@
 // OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
 // IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-
-#ifndef SNARK_FILTER_CONSTANT_POSITION_H
-#define SNARK_FILTER_CONSTANT_POSITION_H
+#pragma once
 
 #include <Eigen/Core>
 
-namespace snark{ 
-namespace constant_position {
+namespace snark { namespace constant_position {
     
 struct state
 {
     state(): covariance( Eigen::Matrix< double, dimension, dimension >::Identity() ) {}
 
-    void set_innovation( const Eigen::Vector3d& innovation )
-    {
-        position += innovation;
-    }
+    void set_innovation( const Eigen::Vector3d& innovation ) { position += innovation; }
 
     static const unsigned int dimension = 3u;
     Eigen::Vector3d position;
     Eigen::Matrix3d covariance;
 };
 
-/// constant position motion model
-struct model
+class model
 {
-    Eigen::Matrix< double, state::dimension, 1 > sigma; /// noise sigma
-    Eigen::Matrix< double, state::dimension, state::dimension > m_jacobian;
-    Eigen::Matrix< double, state::dimension, state::dimension > noise;
+    public:
+        model()
+            : _jacobian( Eigen::Matrix< double, state::dimension, state::dimension >::Identity() )
+            , _noise( Eigen::Matrix< double, state::dimension, state::dimension >::Zero() )
+            , _noise_sigma( Eigen::Matrix< double, state::dimension, 1 >::Ones() )
+        {}
 
-    model(void): sigma( Eigen::Matrix< double, state::dimension, 1 >::Ones() ),
-                 m_jacobian( Eigen::Matrix< double, state::dimension, state::dimension >::Identity() ),
-                 noise( Eigen::Matrix< double, state::dimension, state::dimension >::Zero() )
-    { }
+        Eigen::Matrix< double, state::dimension, state::dimension >& jacobian( const state& state, double dt ) { return _jacobian; }
 
-    Eigen::Matrix< double, state::dimension, state::dimension > & jacobian( const state & state, double dt )
-    {
-        return m_jacobian;
-    }
+        void update_state( state& state, const double dt ) {}
 
-    void update_state( state & state, const double dt ){ }
-
-    Eigen::Matrix< double, state::dimension, state::dimension >& noise_covariance( double dt )
-    {
-        for( unsigned int i = 0; i < 3; i++ )
+        Eigen::Matrix< double, state::dimension, state::dimension >& noise_covariance( double dt )
         {
-            noise(i,i) = dt * sigma[i];
+            for( unsigned int i = 0; i < 3; i++ ) { _noise( i, i ) = dt * sigma[i]; }
+            return noise;
         }
-        return noise;
-    }
 
+    private:
+        Eigen::Matrix< double, state::dimension, state::dimension > _jacobian;
+        Eigen::Matrix< double, state::dimension, state::dimension > _noise;
+        Eigen::Matrix< double, state::dimension, 1 > _noise_sigma;
 };
 
 struct position
@@ -87,29 +76,10 @@ struct position
     Eigen::Matrix3d covariance;
     Eigen::Matrix3d jacobian;
 
-    position(): position( Eigen::Vector3d::Zero() ),
-                covariance( Eigen::Matrix3d::Identity() ),
-                jacobian( Eigen::Matrix3d::Identity() )
-    {
-    }
-
-    const Eigen::Matrix3d& measurement_jacobian( const state & state ) const
-    {
-        return jacobian;
-    }
-
-    const Eigen::Matrix3d measurement_covariance( const state & state ) const
-    {
-        return covariance;
-    }
-
-    const Eigen::Matrix< double, dimension, 1 > innovation( const state & state ) const
-    {
-        return position;
-    }
+    position(): position( Eigen::Vector3d::Zero() ), covariance( Eigen::Matrix3d::Identity() ), jacobian( Eigen::Matrix3d::Identity() ) {}
+    const Eigen::Matrix3d& measurement_jacobian( const state & state ) const { return jacobian; }
+    const Eigen::Matrix3d measurement_covariance( const state & state ) const { return covariance; }
+    const Eigen::Matrix< double, dimension, 1 > innovation( const state & state ) const { return position; }
 };
 
-
-} } }
-
-#endif // SNARK_FILTER_CONSTANT_POSITION_H
+} } // namespace snark { namespace constant_position {
--- math/filter/constant_speed.h
+++ math/filter/constant_speed.h
@@ -28,131 +28,97 @@
 // IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
-#ifndef SNARK_FILTER_SIMPLE_CONSTANT_SPEED_H
-#define SNARK_FILTER_SIMPLE_CONSTANT_SPEED_H
+#pragma once
 
 #include <Eigen/Core>
 
-namespace snark{ 
+namespace snark {
 
 /// point motion model with constant speed    
 template< unsigned int N >
 struct constant_speed
 {
-
-typedef Eigen::Matrix< double, ( N << 1 ), 1 > state_type;
-typedef Eigen::Matrix< double, ( N << 1 ), ( N << 1 ) > covariance_type;
-    
-/// state is of shape [2N x 1] in format: [ position[1:N]; speed[1:N] ]
-struct state
-{
-    state();
-    state( const state_type& s );
-    void set_innovation( const state_type& innovation );
-    
-    static const unsigned int dimension = ( N << 1 );
-    state_type state_vector;
-    covariance_type covariance;
-};
-
-/// motion model
-struct model
-{
-    model( double noiseSigma );
-    model( double noise_sigma_position, double noise_sigma_speed );
-
-    const covariance_type& jacobian( const state & s, double dt );
-
-    void update_state( state & s, double dt ) const;
-
-    const covariance_type& noise_covariance( double dt );
-    
-    state_type sigma; /// noise sigma
-    covariance_type A;
-    covariance_type noise;
-};
-
-/// measurement model: absolute position
-struct position
-{
-    typedef Eigen::Matrix< double, N, 1 > position_type;
-    
-    position( const position_type& p, double sigma );
-
-    const Eigen::Matrix< double, N, ( N << 1 ) >& measurement_jacobian( const state & s ) const
+    typedef Eigen::Matrix< double, ( N << 1 ), 1 > state_type;
+    typedef Eigen::Matrix< double, ( N << 1 ), ( N << 1 ) > covariance_type;
+        
+    /// state is of shape [2N x 1] in format: [ position[1:N]; speed[1:N] ]
+    struct state
     {
-        return jacobian;
-    }
+        static const unsigned int dimension = ( N << 1 );
+        state_type state_vector;
+        covariance_type covariance;
 
-    const Eigen::Matrix< double, N, N >& measurement_covariance( const state & s ) const
-    {
-        return covariance;
-    }
+        state();
+        state( const state_type& s );
+        void set_innovation( const state_type& innovation );
+    };
 
-    position_type innovation( const state& s ) const
+    /// motion model
+    struct model
     {
-        return ( position_vector - jacobian * s.state_vector );
-    }
-    
-    static const int dimension = N;
-    position_type position_vector;
-    Eigen::Matrix< double, N, N > covariance;
-    Eigen::Matrix< double, N, ( N << 1 ) > jacobian;
+        state_type sigma; /// noise sigma
+        covariance_type A;
+        covariance_type noise;
+
+        model( double noise_sigma );
+        model( double noise_sigma_position, double noise_sigma_speed );
+        const covariance_type& jacobian( const state & s, double dt );
+        void update_state( state & s, double dt ) const;
+        const covariance_type& noise_covariance( double dt );
+    };
+
+    /// measurement model: absolute position
+    struct position
+    {
+        typedef Eigen::Matrix< double, N, 1 > position_type;
+
+        static const int dimension = N;
+        position_type position_vector;
+        Eigen::Matrix< double, N, N > covariance;
+        Eigen::Matrix< double, N, ( N << 1 ) > jacobian;
+        
+        position( const position_type& p, double sigma );
+        const Eigen::Matrix< double, N, ( N << 1 ) >& measurement_jacobian( const state & s ) const { return jacobian; }
+        const Eigen::Matrix< double, N, N >& measurement_covariance( const state & s ) const { return covariance; }
+        position_type innovation( const state& s ) const { return position_vector - jacobian * s.state_vector; }
+        
+
+    };
 };
 
-};
-
-/// constructor, sets state and covariance to zero
 template< unsigned int N >
-constant_speed< N >::state::state():
-    state_vector( state_type::Zero() ),
-    covariance( covariance_type::Zero() )
-{
+inline constant_speed< N >::state::state(): state_vector( state_type::Zero() ), covariance( covariance_type::Zero() ) {}
 
-}
-
-/// constructor, sets state and covariance to zero
 template< unsigned int N >
-constant_speed< N >::state::state( const state_type& s ):
-    state_vector( s ),
-    covariance( covariance_type::Zero() )
-{
+inline constant_speed< N >::state::state( const state_type& s ): state_vector( s ), covariance( covariance_type::Zero() ) {}
 
-}
-
-/// update the state from the innovation
 template< unsigned int N >
-void constant_speed< N >::state::set_innovation( const Eigen::Matrix< double, ( N << 1 ), 1 >& innovation )
-{
-    state_vector += innovation;
-}
+inline void constant_speed< N >::state::set_innovation( const Eigen::Matrix< double, ( N << 1 ), 1 >& innovation ) { state_vector += innovation; }
 
 /// constructor
-/// @param noiseSigma process noise for speed and position
+/// @param noise_sigma process noise for speed and position
 template< unsigned int N >
-constant_speed< N >::model::model( double noiseSigma ):
-    sigma( state_type::Ones() * noiseSigma ),
-    A( covariance_type::Identity() ),
-    noise( covariance_type::Zero() )
+inline constant_speed< N >::model::model( double noise_sigma )
+    : sigma( state_type::Ones() * noise_sigma )
+    , A( covariance_type::Identity() )
+    , noise( covariance_type::Zero() )
 {
-
 }
 
 /// constructor
 /// @param noise_sigma_position process noise for position
 /// @param noise_sigma_speed process noise for speed
 template< unsigned int N >
-constant_speed< N >::model::model( double noise_sigma_position, double noise_sigma_speed ):
-    A( covariance_type::Identity() ),
-    noise( covariance_type::Zero() )
+inline constant_speed< N >::model::model( double noise_sigma_position, double noise_sigma_speed )
+    : A( covariance_type::Identity() )
+    , noise( covariance_type::Zero() )
 {
     sigma << Eigen::Matrix< double, N, 1 >::Ones() * noise_sigma_position,
              Eigen::Matrix< double, N, 1 >::Ones() * noise_sigma_speed;
 }
 
-/// get the jacobian of the motion model
 template< unsigned int N >
-const typename constant_speed< N >::covariance_type& constant_speed< N >::model::jacobian( const typename constant_speed< N >::state& s, double dt )
+inline const typename constant_speed< N >::covariance_type& constant_speed< N >::model::jacobian( const typename constant_speed< N >::state& s, double dt )
 {
     A.block( 0, N, N, N ) = Eigen::Matrix< double, N, N >::Identity() * dt;
     return A;
@@ -160,19 +126,12 @@ const typename constant_speed< N >::covariance_type& constant_speed< N >::model:
 
 /// perform prediction update
 template< unsigned int N >
-void constant_speed< N >::model::update_state( state& s, double dt ) const
-{
-    s.state_vector = A * s.state_vector;
-}
+inline void constant_speed< N >::model::update_state( state& s, double dt ) const { s.state_vector = A * s.state_vector; }
 
-/// get process noise covariance
 template< unsigned int N >
-const typename constant_speed< N >::covariance_type& constant_speed< N >::model::noise_covariance( double dt )
+inline const typename constant_speed< N >::covariance_type& constant_speed< N >::model::noise_covariance( double dt )
 {
-    for( unsigned int i = 0; i < ( N << 1 ); i++ )
-    {
-        noise(i,i) = dt * sigma[i];
-    }
+    for( unsigned int i = 0; i < ( N << 1 ); ++i ) { noise(i,i) = dt * sigma[i]; }
     return noise;
 }
 
@@ -180,15 +139,11 @@ const typename constant_speed< N >::covariance_type& constant_speed< N >::model:
 /// @param p measurement position estimate
 /// @param sigma measurement noise
 template< unsigned int N >
-constant_speed< N >::position::position( const position_type& p, double sigma ):
-    position_vector( p ),
-    covariance( Eigen::Matrix< double, N, N >::Identity()*sigma ),
-    jacobian( Eigen::Matrix< double, N, ( N << 1 ) >::Identity() )
+inline constant_speed< N >::position::position( const position_type& p, double sigma )
+    : position_vector( p )
+    , covariance( Eigen::Matrix< double, N, N >::Identity()*sigma )
+    , jacobian( Eigen::Matrix< double, N, ( N << 1 ) >::Identity() )
 {
-
 }
-
     
-} 
-
-#endif // SNARK_FILTER_SIMPLE_CONSTANT_SPEED_H
+} // namespace snark
