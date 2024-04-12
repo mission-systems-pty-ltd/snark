9d44b50129330f770dc2584982eef8b5bb6388ca
vsevolod vlaskine <vsevolod.vlaskine@gmail.com>
2024-04-12 20:13:45 +1000

    cv-calc: graph: in progress...

--- imaging/applications/cv_calc/graph.cpp
+++ imaging/applications/cv_calc/graph.cpp
@@ -26,7 +26,8 @@ std::string options()
     std::ostringstream oss;
     oss << "        --input-fields; print csv input fields to stdin and exit" << std::endl;
     oss << "        --fps=<n>; given frame rate, otherwise redraw on each input on change" << std::endl;
-    oss << "        --svg=<image>; background svg image" << std::endl;
+    oss << "        --list; list svg graph entities" << std::endl;
+    oss << "        --svg=<image>; background svg image created using graphviz dot and transparent fill" << std::endl;
     oss << "        todo" << std::endl;
     return oss.str();
 }
@@ -62,29 +63,75 @@ template <> struct traits< snark::cv_calc::graph::input >
 
 namespace snark { namespace cv_calc { namespace graph {
 
+// static void traverse( boost::property_tree::ptree::const_iterator i
+//                     , bool is_begin
+//                     , xpath& path
+//                     , xpath& display_path
+//                     , property_tree::path_mode mode
+//                     , char equal_sign
+//                     , char delimiter
+//                     , const std::string& root
+//                     , bool const unquote_numbers )
+// {
+//     if( i->second.begin() == i->second.end() )
+//     {
+//         if( os ) { ptree_output_value_( *os, i->second.get_value< std::string >(), is_begin, display_path / i->first, equal_sign, delimiter, root, unquote_numbers ); }
+//         else { pv.push_back( std::make_pair( display_path / i->first, i->second.get_value< std::string >() ) ); }
+//     }
+//     else
+//     {
+//         path /= i->first;
+//         display_path /= i->first;
+//         boost::optional< std::string > v = i->second.get_value_optional< std::string >();
+//         if( v ) // quick and dirty
+//         {
+//             const std::string& stripped = comma::strip( *v );
+//             if( !stripped.empty() )
+//             { 
+//                 if( os ) { ptree_output_value_( *os, stripped, is_begin, display_path, equal_sign, delimiter, root, unquote_numbers ); }
+//                 else { pv.push_back( std::make_pair( display_path, stripped ) ); }
+//             }
+//         }
+//         comma::uint32 index=0;
+//         for( boost::property_tree::ptree::const_iterator j = i->second.begin(); j != i->second.end(); ++j )
+//         {
+//             // Test if it is json array data, if so all keys are empty. If so display indices in path if requested
+//             if( mode == property_tree::without_brackets && j->first.empty()  ) { display_path /= boost::lexical_cast< std::string >( index++ ); }
+//             else if( mode == property_tree::with_brackets && j->first.empty() ) { display_path.elements.back().index = index++; }
+//             ptree_to_path_value_string_impl( os, pv, j, is_begin, path, display_path, mode, equal_sign, delimiter, root, unquote_numbers );
+//             if( mode == property_tree::without_brackets && j->first.empty() ) { display_path = display_path.head(); }
+//             is_begin = false;
+//         }
+//         if( !(i->first.empty()) ) { path = path.head(); display_path = display_path.head(); } // for json arrays, the keys are empty
+//     }
+// }
+
 int run( const comma::command_line_options& options )
 {
     if( options.exists( "--input-fields" ) ) { std::cout << comma::join( comma::csv::names< input >(), ',' ) << std::endl; return 0; }
-    std::string output_options_string = options.value< std::string >( "--output", "" );
-    snark::cv_mat::serialization::options output_options = comma::name_value::parser( ';', '=' ).get< snark::cv_mat::serialization::options >( output_options_string );
-    snark::cv_mat::serialization output_serialization( output_options );
-    comma::csv::options csv( options );
-    bool has_block = csv.fields.empty() || csv.has_field( "block" );
-    unsigned int fps = options.value( "--fps", 0 );
     std::string filename = options.value< std::string >( "--svg" );
-    cv::Mat svg, image;
-    cv::VideoCapture capture;
-    capture.open( filename );
-    capture >> svg;
-    svg.copyTo( image );
-    //cv::imshow( "blah", svg );
-    //cv::waitKey( 0 );
     boost::property_tree::ptree t;
     {
         std::ifstream ifs( filename );
         COMMA_ASSERT_BRIEF( ifs.is_open(), "failed to open svg file '" << filename << "'" );
         boost::property_tree::read_xml( ifs, t );
     }
+    if( options.exists( "--list" ) )
+    {
+        // todo
+        return 0;
+    }
+    std::string output_options_string = options.value< std::string >( "--output", "" );
+    snark::cv_mat::serialization::options output_options = comma::name_value::parser( ';', '=' ).get< snark::cv_mat::serialization::options >( output_options_string );
+    snark::cv_mat::serialization output_serialization( output_options );
+    comma::csv::options csv( options );
+    bool has_block = csv.fields.empty() || csv.has_field( "block" );
+    unsigned int fps = options.value( "--fps", 0 );
+    cv::Mat svg;
+    cv::VideoCapture capture;
+    capture.open( filename );
+    capture >> svg;
+    cv::Mat canvas = cv::Mat::zeros( svg.rows, svg.cols, CV_8UC3 );
     std::unordered_map< std::uint32_t, input > previous;
     std::unordered_map< std::uint32_t, input > inputs;
     comma::csv::input_stream< input > istream( std::cin, csv );
