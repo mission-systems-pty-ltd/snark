24d4311afc165ad7acf2e42128814300c474257f
seva <seva@tinbox>
2024-04-12 15:04:14 +1000

    cv-calc: graph operation: in progress...

--- imaging/applications/cv_calc/graph.cpp
+++ imaging/applications/cv_calc/graph.cpp
@@ -2,20 +2,18 @@
 
 /// @author vsevolod vlaskine
 
+#include <fstream>
 #include <sstream>
 #include <unordered_map>
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/xml_parser.hpp>
 #include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
-// #include <opencv2/core/version.hpp>
-// #if defined( CV_VERSION_EPOCH ) && CV_VERSION_EPOCH == 2
-// #include <opencv2/calib3d/calib3d.hpp>
-// #else
-// #include <opencv2/calib3d.hpp>
-// #endif
 #include <comma/base/exception.h>
+#include <comma/csv/names.h>
 #include <comma/csv/stream.h>
+#include <comma/string/string.h>
 #include <comma/visiting/traits.h>
 #include "graph.h"
 
@@ -64,13 +62,25 @@ namespace snark { namespace cv_calc { namespace graph {
 
 int run( const comma::command_line_options& options, const snark::cv_mat::serialization& input_options, const snark::cv_mat::serialization& output_options )
 {
+    if( options.exists( "--input-fields" ) ) { std::cout << comma::join( comma::csv::names< input >(), ',' ) << std::endl; return 0; }
     snark::cv_mat::serialization input_serialization( input_options ); ( void )( input_serialization ); // in case we later decide to have svg input stream or alike
     snark::cv_mat::serialization output_serialization( output_options );
     comma::csv::options csv( options );
-    comma::csv::input_stream< input > istream( std::cin, csv );
-    bool has_block = csv.has_field( "block" );
+    bool has_block = csv.fields.empty() || csv.has_field( "block" );
+    unsigned int fps = options.value( "--fps", 0 );
+    std::string filename = options.value< std::string >( "--svg" );
+    cv::Mat svg = cv::imread( filename, cv::IMREAD_UNCHANGED );
+    std::cerr << "==> svg: rows: " << svg.rows << " cols: " << svg.cols << std::endl;
+    boost::property_tree::ptree t;
+    {
+        std::ifstream ifs( filename );
+        COMMA_ASSERT_BRIEF( ifs.is_open(), "failed to open svg file '" << filename << "'" );
+        boost::property_tree::read_xml( ifs, t );
+    }
     std::unordered_map< std::uint32_t, input > previous;
     std::unordered_map< std::uint32_t, input > inputs;
+    comma::csv::input_stream< input > istream( std::cin, csv );
+    boost::posix_time::ptime deadline;
     while( std::cin.good() || istream.ready() )
     {
         auto p = istream.read();
@@ -78,6 +88,13 @@ int run( const comma::command_line_options& options, const snark::cv_mat::serial
         {
             // todo: fps
             // todo: changed?
+            cv::Mat image = svg;
+            auto now = boost::posix_time::microsec_clock::universal_time();
+            if( deadline.is_not_a_date_time() || now >= deadline )
+            {
+                output_serialization.write_to_stdout( std::make_pair( now, svg ) );
+                if( fps > 0 ) { deadline = now + boost::posix_time::microseconds( long( 1000000. / fps ) ); }
+            }
             previous = std::move( inputs );
             inputs.clear();
         }
