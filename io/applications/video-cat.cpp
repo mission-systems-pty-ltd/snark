ad170fa3283dbfbcf02d0fbf8e83c3c813a9cc0b
seva <seva@tinbox>
2024-05-24 13:01:34 +1000

    video-cat: --number-of-threads implemented

--- io/applications/video-cat.cpp
+++ io/applications/video-cat.cpp
@@ -28,6 +28,7 @@ options
     --height=<rows>
     --pixel-type=<type>; default=rggb; todo...
     --size,--number-of-buffers=<n>; default=32
+    --threads,-number-of-threads=<n>; default: run with --output-number-of-thread-default
     --width=<bytes>
 output options
     --fields=[<fields>]; header output fields: t,width,height,type,count
@@ -41,6 +42,7 @@ output options
     --output-header-fields,--output-fields
     --output-header-format,--output-format
     --output-header-only,--header-only; output header only, e.g. for debugging
+    --output-number-of-thread-default
 
 )";
     exit( 0 );
@@ -110,7 +112,8 @@ int main( int ac, char** av )
         comma::command_line_options options( ac, av, usage );
         if( options.exists( "--output-header-fields,--output-fields" ) ) { std::cout << comma::join( comma::csv::names< snark::io::video::header >(), ',' ) << std::endl; return 0; }
         if( options.exists( "--output-header-format,--output-format" ) ) { std::cout << comma::csv::format::value< snark::io::video::header >() << std::endl; return 0; }
-        const auto& unnamed = options.unnamed( "--discard,--output-header-fields,--output-fields,--output-header-format,--output-format,--output-header-only,--header-only", "-.*" );
+        if( options.exists( "--output-number-of-thread-default" ) ) { std::cout << snark::tbb::default_concurrency() << std::endl; return 0; }
+        const auto& unnamed = options.unnamed( "--discard,--output-header-fields,--output-fields,--output-header-format,--output-format,--output-header-only,--header-only,--output-number-of-thread-default", "-.*" );
         COMMA_ASSERT_BRIEF( !unnamed.empty(), "please specify video device" );
         COMMA_ASSERT_BRIEF( unnamed.size() <= 2, "expected one video device; got'" << comma::join( unnamed, ' ' ) << "'" );
         auto name = unnamed[0];
@@ -135,6 +138,7 @@ int main( int ac, char** av )
         snark::io::video::header header;
         unsigned int width = options.value< unsigned int >( "--width" );
         unsigned int height = options.value< unsigned int >( "--height" );
+        unsigned int number_of_threads = options.value( "--thread,--number-of-threads", snark::tbb::default_concurrency() );
         unsigned int pixel_size = 4;
         header.width = width / pixel_size;
         header.height = height;
@@ -176,8 +180,8 @@ int main( int ac, char** av )
         comma::saymore() << name << ": video stream: starting..." << std::endl;
         video.start();
         comma::saymore() << name << ": video stream: started" << std::endl;
-        comma::saymore() << name << ": processing pipeline: running..." << std::endl;
-        ::tbb::parallel_pipeline( video.buffers().size() + 1, filters ); // while( bursty_reader->wait() ) { ::tbb::parallel_pipeline( 3, filters ); }
+        comma::saymore() << name << ": processing pipeline: running with maximum number of active tokens " << number_of_threads << "..." << std::endl;
+        ::tbb::parallel_pipeline( number_of_threads, filters ); // ::tbb::parallel_pipeline( video.buffers().size() + 1, filters ); // while( bursty_reader->wait() ) { ::tbb::parallel_pipeline( 3, filters ); }
         comma::saymore() << name << ": processing pipeline: stopped" << std::endl;
         video.stop();
         comma::saymore() << name << ": video stream: stopped" << std::endl;
