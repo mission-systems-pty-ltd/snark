632c44177ac8ab5f04899487f9c1a9a8b7e36dc5
seva <seva@tinbox>
2024-08-07 16:14:34 +1000

    image-from-csv: sliding window example: colours fixed

--- imaging/applications/image-from-csv.cpp
+++ imaging/applications/image-from-csv.cpp
@@ -159,15 +159,15 @@ fields: t,x,y,r,g,b,block or t,x,y,grey,block
             | image-from-csv --fields block,id,y,x \
                              --shape lines \
                              --output 'rows=380;cols=400;type=3ub' \
-                             --colours="magenta;cyan;yellow;white" \
+                             --colours="magenta;cyan;yellow;red" \
             | cv-cat 'canvas=420,400,10,10' \
                      'draw=grid,10,10,100,60,400,360,100,100,100,1' \
                      'text=0,10,390,light-grey,scale:0.4' \
                      'text=100,380,390,light-grey,scale:0.4' \
-                     'rectangle=110,378,160,394,255,0,0,-1,-1,0,128' \
-                     'rectangle=160,378,210,394,0,200,0,-1,-1,0,128' \
-                     'rectangle=210,378,260,394,150,150,255,-1,-1,0,128' \
-                     'rectangle=260,378,310,394,255,255,0,-1,-1,0,128' \
+                     'rectangle=110,378,160,394,0,255,255,-1,-1,0,128' \
+                     'rectangle=160,378,210,394,255,,255,-1,-1,0,128' \
+                     'rectangle=210,378,260,394,255,255,0,-1,-1,0,128' \
+                     'rectangle=260,378,310,394,255,0,0,-1,-1,0,128' \
                      'text=A,130,390,light-grey,scale:0.4' \
                      'text=B,180,390,light-grey,scale:0.4' \
                      'text=C,230,390,light-grey,scale:0.4' \
