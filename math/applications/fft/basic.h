dc3814568e71db84fc22598517d489cc68250bd8
vsevolod vlaskine <vsevolod.vlaskine@gmail.com>
2025-01-13 10:41:15 +1100

    math-fft: refactored in preparation of semi-deprecating the current use

---
+++ math/applications/fft/basic.h
@@ -0,0 +1,13 @@
+// Copyright (c) 2025 Vsevolod Vlaskine
+
+#pragma once
+
+#include <comma/application/command_line_options.h>
+
+namespace snark { namespace math { namespace applications { namespace fft { namespace basic {
+
+std::string usage( bool verbose );
+
+int run( const comma::command_line_options& options );
+
+} } } } } // namespace snark { namespace math { namespace applications { namespace fft { namespace basic {
