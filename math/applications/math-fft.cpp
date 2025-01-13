adf03f513b66fd902ea4a86056742878dc73b783
vsevolod vlaskine <vsevolod.vlaskine@gmail.com>
2025-01-13 11:37:56 +1100

    math-fft: current implementation requires --basic now

--- math/applications/math-fft.cpp
+++ math/applications/math-fft.cpp
@@ -7,7 +7,7 @@
 #include <fftw3.h>
 #include <comma/visiting/traits.h>
 #include <comma/application/command_line_options.h>
-#include <comma/application/verbose.h>
+#include <comma/base/exception.h>
 #include <comma/csv/options.h>
 #include <comma/csv/stream.h>
 #include <comma/string/string.h>
@@ -38,7 +38,8 @@ int main( int ac, char** av )
     {
         comma::command_line_options options( ac, av );
         if( options.exists( "--help,-h" ) ) { usage( options ); }
-        return snark::math::applications::fft::basic::run( options );
+        if( options.exists( "--basic" ) ) { return snark::math::applications::fft::basic::run( options ); }
+        COMMA_THROW_BRIEF( comma::exception, "please specify --basic; extended functionality: in progress..." );
     }
     catch( std::exception& ex ) { comma::say() << ": " << ex.what() << std::endl; }
     catch( ... ) { comma::say() << ": " << "unknown exception" << std::endl; }
