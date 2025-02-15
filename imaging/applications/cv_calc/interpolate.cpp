8a8e2fa4b7559e30798b2d731714efeb304d8f69
vsevolod vlaskine <vsevolod.vlaskine@gmail.com>
2025-02-16 00:20:05 +1100

    cv-calc: interpolate: in progress...

--- imaging/applications/cv_calc/interpolate.cpp
+++ imaging/applications/cv_calc/interpolate.cpp
@@ -24,29 +24,26 @@ int run( const comma::command_line_options& options )
     unsigned int radius = options.value( "--radius", 0u );
     unsigned int steps = options.value< unsigned int >( "--steps" );
     typedef std::pair< boost::posix_time::ptime, cv::Mat > pair_t;
-    pair_t previous;
-    pair_t next;
+    pair_t previous, next;
     while( std::cin.good() )
     {
         pair_t p = input_serialization.read< boost::posix_time::ptime >( std::cin );
         if( p.second.empty() ) { return 0; }
-        if( previous.second.empty() )
-        {
-            previous = p;
-        }
-        else
+        if( !previous.second.empty() )
         {
             COMMA_ASSERT_BRIEF( p.second.type() == previous.second.type(), "expected images of the same type " << previous.second.type() << "; got: " << p.second.type() );
             if( next.second.empty() ) { next.second = cv::Mat( p.second.rows, p.second.cols, p.second.type() ); }
             boost::posix_time::time_duration dt;
-            if( !p.first.is_not_a_date_time() && !previous.first.is_not_a_date_time() ) { dt = ( p.first - previous.first ) / steps; }
+            if( !p.first.is_not_a_date_time() && !previous.first.is_not_a_date_time() ) { dt = ( p.first - previous.first ) / ( steps + 1 ); }
             next.first = previous.first + dt;
             for( unsigned int i = 1; i < steps; ++i, next.first += dt )
             {
                 ( void )radius;
+
                 // todo: interpolate pixels
-                // todo: interpolate timestamp
+                
                 output_serialization.write_to_stdout( next, true );
+                previous = next;
             }
         }
         previous = p;
