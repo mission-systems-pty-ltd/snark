91a74739072ea0164a26c40831b77286c4c679ac
rex <rex@super>
2025-03-11 13:35:14 +1100

    io/video: allow user defined buffer to be passed to video stream

--- io/video.h
+++ io/video.h
@@ -25,7 +25,8 @@ class stream
             record( std::uint32_t count, const snark::timestamped< void* >& buffer ): count( count ), buffer( buffer ) {}
             operator bool() const { return buffer.data != nullptr; }
         };
-        stream( const std::string& name, unsigned int width, unsigned int height, unsigned int number_of_buffers, int pixel_format = V4L2_PIX_FMT_Y16 );
+        stream( const std::string& name, unsigned int width, unsigned int height, std::vector< snark::timestamped< void* > >* user_buffers, int pixel_format ); // USERPTR constructor
+        stream( const std::string& name, unsigned int width, unsigned int height, unsigned int number_of_buffers, int pixel_format = V4L2_PIX_FMT_Y16 ); // IOMAP constructor
         ~stream();
         const unsigned int width() const { return _width; }
         const unsigned int height() const { return _height; }
@@ -33,19 +34,28 @@ class stream
         void start();
         void stop();
         record read( float timeout = 1, unsigned int attempts = 1 );
-        const std::vector< snark::timestamped< void* > >& buffers() const { return _buffers; }
+        const std::vector< snark::timestamped< void* > >& buffers() const 
+        {   
+            if(_buffers == nullptr) { COMMA_THROW(comma::exception, "_buffers is nullptr"); }
+            return *_buffers; 
+        }
 
     private:
+        enum io_method_t { IO_MMAP, IO_USERPTR }; // todo: add support for DMABUF?
+        io_method_t _io_method = IO_MMAP;   // preserve existing functionality by assuming IO_MMAP is default
         std::string _name;
         unsigned int _width{0};
         unsigned int _height{0};
         std::FILE* _file{nullptr};
         int _fd{-1};
-        std::vector< snark::timestamped< void* > > _buffers;
+        std::vector< snark::timestamped< void* > >* _buffers{nullptr};
         unsigned int _size{0};
         unsigned int _index{0};
         unsigned int _count{0};
         bool _started{false};
+
+        void initialise_stream( const std::string& name, const unsigned int width, const unsigned int height, const unsigned int number_of_buffers, const int pixel_format );
+
 
 };
 
