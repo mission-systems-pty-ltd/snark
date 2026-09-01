2fcdc62cc5761010d7c8fdfed9ce282f18acc0c7
vsevolod vlaskine <vsevolod.vlaskine@gmail.com>
2026-09-01 14:14:33 +1000

    imaging/applications/cv_calc/aruco.cpp: compilation fixed for opencv 4.5

--- imaging/applications/cv_calc/aruco.cpp
+++ imaging/applications/cv_calc/aruco.cpp
@@ -2,6 +2,7 @@
 
 /// @author vsevolod vlaskine
 
+#include <sstream>
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include <opencv2/opencv.hpp>
 #include <opencv2/calib3d.hpp>
@@ -26,10 +27,10 @@ namespace snark { namespace cv_calc { namespace aruco { namespace detection {
 
 std::string options()
 {
+    std::ostringstream oss;
     #if CV_MAJOR_VERSION < 4 || ( CV_MAJOR_VERSION == 4 && CV_MINOR_VERSION <= 5 )
     oss << "    cv-calc built with opencv " << CV_VERSION << ", which does not support aruco detection" << std::endl;
     #endif
-    std::ostringstream oss;
     oss << R"(        --dictionary,--dict=<dictionary>
         --pinhole-config,--pinhole=<config>; <config>: <filename>[:<path>]
         --output-dictionaries,--dictionaries; output list of dictionary names to stdout and exit
@@ -66,33 +67,38 @@ struct output
 
 struct dictionaries
 {
-    static const std::map< std::string, cv::aruco::PredefinedDictionaryType >& types()
+    #if CV_MAJOR_VERSION == 4 && CV_MINOR_VERSION == 5
+        using type = cv::aruco::PREDEFINED_DICTIONARY_NAME;
+    #else
+        using type = cv::aruco::PredefinedDictionaryType;
+    #endif
+    static const std::map< std::string, type >& types()
     {
-        static const std::map< std::string, cv::aruco::PredefinedDictionaryType > t =   { { "4X4_50", cv::aruco::DICT_4X4_50 }
-                                                                                        , { "4X4_100", cv::aruco::DICT_4X4_100 }
-                                                                                        , { "4X4_250", cv::aruco::DICT_4X4_250 }
-                                                                                        , { "4X4_1000", cv::aruco::DICT_4X4_1000 }
-                                                                                        , { "5X5_50", cv::aruco::DICT_5X5_50 }
-                                                                                        , { "5X5_100", cv::aruco::DICT_5X5_100 }
-                                                                                        , { "5X5_250", cv::aruco::DICT_5X5_250 }
-                                                                                        , { "5X5_1000", cv::aruco::DICT_5X5_1000 }
-                                                                                        , { "6X6_50", cv::aruco::DICT_6X6_50 }
-                                                                                        , { "6X6_100", cv::aruco::DICT_6X6_100 }
-                                                                                        , { "6X6_250", cv::aruco::DICT_6X6_250 }
-                                                                                        , { "6X6_1000", cv::aruco::DICT_6X6_1000 }
-                                                                                        , { "7X7_50", cv::aruco::DICT_7X7_50 }
-                                                                                        , { "7X7_100", cv::aruco::DICT_7X7_100 }
-                                                                                        , { "7X7_250", cv::aruco::DICT_7X7_250 }
-                                                                                        , { "7X7_1000", cv::aruco::DICT_7X7_1000 }
-                                                                                        , { "ARUCO_ORIGINAL", cv::aruco::DICT_ARUCO_ORIGINAL }
-                                                                                        , { "APRILTAG_16h5", cv::aruco::DICT_APRILTAG_16h5 }
-                                                                                        , { "APRILTAG_25h9", cv::aruco::DICT_APRILTAG_25h9 }
-                                                                                        , { "APRILTAG_36h10", cv::aruco::DICT_APRILTAG_36h10 }
-                                                                                        , { "APRILTAG_36h11", cv::aruco::DICT_APRILTAG_36h11 }
-                                                                                        , { "ARUCO_MIP_36h12", cv::aruco::DICT_ARUCO_MIP_36h12 } };
+        static const std::map< std::string, type > t =  { { "4X4_50", cv::aruco::DICT_4X4_50 }
+                                                        , { "4X4_100", cv::aruco::DICT_4X4_100 }
+                                                        , { "4X4_250", cv::aruco::DICT_4X4_250 }
+                                                        , { "4X4_1000", cv::aruco::DICT_4X4_1000 }
+                                                        , { "5X5_50", cv::aruco::DICT_5X5_50 }
+                                                        , { "5X5_100", cv::aruco::DICT_5X5_100 }
+                                                        , { "5X5_250", cv::aruco::DICT_5X5_250 }
+                                                        , { "5X5_1000", cv::aruco::DICT_5X5_1000 }
+                                                        , { "6X6_50", cv::aruco::DICT_6X6_50 }
+                                                        , { "6X6_100", cv::aruco::DICT_6X6_100 }
+                                                        , { "6X6_250", cv::aruco::DICT_6X6_250 }
+                                                        , { "6X6_1000", cv::aruco::DICT_6X6_1000 }
+                                                        , { "7X7_50", cv::aruco::DICT_7X7_50 }
+                                                        , { "7X7_100", cv::aruco::DICT_7X7_100 }
+                                                        , { "7X7_250", cv::aruco::DICT_7X7_250 }
+                                                        , { "7X7_1000", cv::aruco::DICT_7X7_1000 }
+                                                        , { "ARUCO_ORIGINAL", cv::aruco::DICT_ARUCO_ORIGINAL } };
+                                                        // , { "APRILTAG_16h5", cv::aruco::DICT_APRILTAG_16h5 }
+                                                        // , { "APRILTAG_25h9", cv::aruco::DICT_APRILTAG_25h9 }
+                                                        // , { "APRILTAG_36h10", cv::aruco::DICT_APRILTAG_36h10 }
+                                                        // , { "APRILTAG_36h11", cv::aruco::DICT_APRILTAG_36h11 }
+                                                        // , { "ARUCO_MIP_36h12", cv::aruco::DICT_ARUCO_MIP_36h12 } };
         return t;
     }
-    static cv::aruco::PredefinedDictionaryType type_from_string( const std::string& name )
+    static type type_from_string( const std::string& name )
     {
         auto i = types().find( name );
         COMMA_ASSERT_BRIEF( i != types().end(), "expected a dictionary name, got: '" << name << "'" );
@@ -158,58 +164,59 @@ int run( const comma::command_line_options& options, const snark::cv_mat::serial
 {
     #if CV_MAJOR_VERSION < 4 || ( CV_MAJOR_VERSION == 4 && CV_MINOR_VERSION <= 5 )
         COMMA_THROW_BRIEF( comma::exception, "cv-calc built with opencv " << CV_VERSION << ", which does not support aruco detection" );
-    #endif
-    if( options.exists( "--output-fields" ) ) { std::cout << comma::join( comma::csv::names< output >(), ',' ) << std::endl; return 0; };
-    if( options.exists( "--output-format" ) ) { std::cout << comma::csv::format( comma::csv::format::value< output >() ).collapsed_string() << std::endl; return 0; }
-    if( options.exists( "--output-dictionaries,--dictionaries" ) ) { for( const auto& t: dictionaries::types() ) { std::cout << t.first << "," << t.second << std::endl; } return 0; }
-    snark::cv_mat::serialization input( input_options );
-    comma::csv::options csv( options );
-    comma::csv::output_stream< output > ostream( std::cout, csv );
-    bool flush = options.exists( "--flush" );
-    bool has_corners = csv.fields.empty() || csv.has_paths( "corners" );
-    bool has_pose = csv.fields.empty() || csv.has_paths( "pose" );
-    #if CV_MAJOR_VERSION == 4 && CV_MINOR_VERSION <= 5
-        COMMA_THROW( comma::exception, "opencv " << CV_VERSION << ": todo soon"  );
     #else
-        cv::aruco::Dictionary dictionary = cv::aruco::getPredefinedDictionary( dictionaries::type_from_string( options.value< std::string >( "--dictionary,--dict" ) ) );
-        cv::aruco::DetectorParameters params = cv::aruco::DetectorParameters();
-        cv::aruco::ArucoDetector detector( dictionary, params );
-        double marker_length = options.value( "--marker-length", 0. );
-        cv::Mat camera_matrix{}, distortion_coeffs{};
-        if( has_pose )
-        {
-            COMMA_ASSERT_BRIEF( marker_length > 0, "please specify --marker-length" );
-            auto s = options.value< std::string >( "--pinhole-config,--pinhole" );
-            const auto& v = comma::split( options.value< std::string >( "--pinhole-config,--pinhole" ), ':', true );
-            COMMA_ASSERT_BRIEF( v.size() == 1 || v.size() == 2, "expected --pinhole-config=<file>[:<path>]; got: '" << s << "'" );
-            const auto& config = comma::read_json< snark::camera::pinhole::config_t >( v[0], v.size() == 2 ? v[1] : "" );
-            camera_matrix = config.camera_matrix();
-            if( config.distortion ) { distortion_coeffs = config.distortion->as< cv::Mat >(); }
-        }
-        output o;
-        std::vector< std::vector< cv::Point2f > > corners;
-        std::vector< int > markers;
-        std::vector< std::vector< cv::Point2f > > rejected;
-        std::vector< cv::Vec3d > rvecs, tvecs;
-        for( ; std::cin.good() && !std::cin.eof(); ++o.block )
-        {
-            std::pair< boost::posix_time::ptime, cv::Mat > p = input.read< boost::posix_time::ptime >( std::cin );
-            if( p.second.empty() ) { return 0; }
-            detector.detectMarkers( p.second, corners, markers, rejected );
-            if( has_pose ) { estimate_poses( corners, marker_length, camera_matrix, distortion_coeffs, rvecs, tvecs ); }
-            for( unsigned i = 0; i < markers.size(); ++i )
+        if( options.exists( "--output-fields" ) ) { std::cout << comma::join( comma::csv::names< output >(), ',' ) << std::endl; return 0; };
+        if( options.exists( "--output-format" ) ) { std::cout << comma::csv::format( comma::csv::format::value< output >() ).collapsed_string() << std::endl; return 0; }
+        if( options.exists( "--output-dictionaries,--dictionaries" ) ) { for( const auto& t: dictionaries::types() ) { std::cout << t.first << "," << t.second << std::endl; } return 0; }
+        snark::cv_mat::serialization input( input_options );
+        comma::csv::options csv( options );
+        comma::csv::output_stream< output > ostream( std::cout, csv );
+        bool flush = options.exists( "--flush" );
+        bool has_corners = csv.fields.empty() || csv.has_paths( "corners" );
+        bool has_pose = csv.fields.empty() || csv.has_paths( "pose" );
+        #if CV_MAJOR_VERSION == 4 && CV_MINOR_VERSION < 5
+            COMMA_THROW( comma::exception, "opencv " << CV_VERSION << ": todo soon"  );
+        #else
+            cv::aruco::Dictionary dictionary = cv::aruco::getPredefinedDictionary( dictionaries::type_from_string( options.value< std::string >( "--dictionary,--dict" ) ) );
+            cv::aruco::DetectorParameters params = cv::aruco::DetectorParameters();
+            cv::aruco::ArucoDetector detector( dictionary, params );
+            double marker_length = options.value( "--marker-length", 0. );
+            cv::Mat camera_matrix{}, distortion_coeffs{};
+            if( has_pose )
             {
-                o.t = p.first;
-                o.id = i;
-                o.marker = markers[i];
-                if( has_corners ) { for( unsigned int j = 0; j < corners[i].size(); ++j ) { o.corners[j] = corners[i][j]; } }
-                if( has_pose ) { o.pose = snark::pose( Eigen::Vector3d( tvecs[i][0], tvecs[i][1], tvecs[i][2] ), roll_pitch_yaw::from_rodriques( tvecs[i][0], tvecs[i][1], tvecs[i][2] ) ); }
-                ostream.write( o );
+                COMMA_ASSERT_BRIEF( marker_length > 0, "please specify --marker-length" );
+                auto s = options.value< std::string >( "--pinhole-config,--pinhole" );
+                const auto& v = comma::split( options.value< std::string >( "--pinhole-config,--pinhole" ), ':', true );
+                COMMA_ASSERT_BRIEF( v.size() == 1 || v.size() == 2, "expected --pinhole-config=<file>[:<path>]; got: '" << s << "'" );
+                const auto& config = comma::read_json< snark::camera::pinhole::config_t >( v[0], v.size() == 2 ? v[1] : "" );
+                camera_matrix = config.camera_matrix();
+                if( config.distortion ) { distortion_coeffs = config.distortion->as< cv::Mat >(); }
             }
-            if( flush ) { std::cout.flush(); }
-        }
+            output o;
+            std::vector< std::vector< cv::Point2f > > corners;
+            std::vector< int > markers;
+            std::vector< std::vector< cv::Point2f > > rejected;
+            std::vector< cv::Vec3d > rvecs, tvecs;
+            for( ; std::cin.good() && !std::cin.eof(); ++o.block )
+            {
+                std::pair< boost::posix_time::ptime, cv::Mat > p = input.read< boost::posix_time::ptime >( std::cin );
+                if( p.second.empty() ) { return 0; }
+                detector.detectMarkers( p.second, corners, markers, rejected );
+                if( has_pose ) { estimate_poses( corners, marker_length, camera_matrix, distortion_coeffs, rvecs, tvecs ); }
+                for( unsigned i = 0; i < markers.size(); ++i )
+                {
+                    o.t = p.first;
+                    o.id = i;
+                    o.marker = markers[i];
+                    if( has_corners ) { for( unsigned int j = 0; j < corners[i].size(); ++j ) { o.corners[j] = corners[i][j]; } }
+                    if( has_pose ) { o.pose = snark::pose( Eigen::Vector3d( tvecs[i][0], tvecs[i][1], tvecs[i][2] ), roll_pitch_yaw::from_rodriques( tvecs[i][0], tvecs[i][1], tvecs[i][2] ) ); }
+                    ostream.write( o );
+                }
+                if( flush ) { std::cout.flush(); }
+            }
+        #endif
+        return 0;
     #endif
-    return 0;
 }
 
 } } } } // namespace snark { namespace cv_calc { namespace aruco { namespace detection {
